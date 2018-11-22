e33be032678a26f110b1ce10b81995e53f5864a8
Erin Trainor <trainor.erin@gmail.com>
2018-11-21 19:46:08 -0800

    added render function

--- js/app.js
+++ js/app.js
@@ -11,17 +11,39 @@ function HornyImage(obj) {
 }
 
 const allHornyImages = [];
+
+HornyImage.prototype.render = function() {
+  $('main').append()('<section class = "clone"></section>');
+  let $clone = $('section [class = "clone"]');
+
+  let hornyTemplate = $('#photo-template').html();
+
+  $clone.html(hornyTemplate);
+
+  $clone.find('h2').text(this.title);
+  $clone.find('p').text(this.description);
+  $clone.find('img').attr('src', this.image_url);
+
+  $clone.removeClass('clone');
+  $clone.attr('class', this.title);
+}
+
 function readJson() {
   $.get('data/page-1.json', 'json').then(data => {
     data.forEach(hornyImageObj => {
       new HornyImage(hornyImageObj)
+      console.log('horny images!')
     })
   }).then(() => {
     allHornyImages.forEach(horn => {
-      console.log(horn);
+      horn.render();
     })
   })
 }
+
+$(() => readJson());
+
+// readJson();
 
 // $(document).ready(function() {
 //   $.get("data/page-1.json")
